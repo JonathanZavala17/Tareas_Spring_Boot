1f267eccfbe1e3a71f5d4bb2e9379463a2f71509
JonathanRivasX370 <37159701+JonathanZavala17@users.noreply.github.com>
2023-08-30 21:49:53 -0600

    prueba inicial de springboot

---
+++ src/main/java/pe/todotic/tareas/controller/TareaController.java
@@ -0,0 +1,52 @@
+package pe.todotic.tareas.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import pe.todotic.tareas.model.Tarea;
+import pe.todotic.tareas.repo.TareaRepository;
+
+import java.util.List;
+
+@CrossOrigin
+@RestController
+@Controller
+@RequestMapping("/tareas")
+public class TareaController {
+    @Autowired
+    private TareaRepository tareaRepositary;
+
+    @GetMapping("/leer")
+    List<Tarea> index() {
+        return tareaRepositary.findAll();
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/guardar")
+    Tarea create(@RequestBody Tarea tarea){
+        return tareaRepositary.save(tarea);
+    }
+
+    @PutMapping("/editar/{id}")
+    Tarea update (@PathVariable String id, @RequestBody Tarea tarea){
+        Tarea tareaFromDB = tareaRepositary
+                .findById(id)
+                .orElseThrow(RuntimeException::new);
+
+        tareaFromDB.setNombre(tarea.getNombre());
+        tareaFromDB.setCompletado(tarea.isCompletado());
+
+        return tareaRepositary.save(tareaFromDB);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping ("/eliminar/{id}")
+    void delete (@PathVariable  String id) {
+            Tarea tarea = tareaRepositary
+                    .findById(id)
+                    .orElseThrow(RuntimeException::new);
+            tareaRepositary.delete(tarea);
+        }
+}
